17ad2bc903212c48bbfc506b0f25d4769a39dd11
justiniyke29 <106402840+justiniyke29@users.noreply.github.com>
2022-11-01 11:23:12 +0100

    added a form component

--- server/index.js
+++ server/index.js
@@ -6,12 +6,12 @@ import cors from 'cors';
 import postRoutes from './routes/posts.js';
 
 const app = express();
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
+app.use('/posts', postRoutes);
 const CONNECTION_URL = 'mongodb+srv://justiniyke29:mummypet29@cluster0.zyqc5xg.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
