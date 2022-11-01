78f82c01ea4b1992166d3bdddbb6741b3a9e60dc
justiniyke29 <106402840+justiniyke29@users.noreply.github.com>
2022-11-01 10:21:33 +0100

    added redux state management

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect} from 'react';
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
-import memories from './images/memories.png'
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts';
+import memories from './images/memories.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import useStyles from './styles';
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch])
   return  (
     <Container maxWidth='lg'>
       <AppBar className={classes.appBar} position='static' color='inherit'>
@@ -15,7 +22,7 @@ const App = () => {
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify="space-between" alignItems='stretch' spacing={3} >
+          <Grid container justifyContent = "space-between" alignItems='stretch' spacing={3} >
             <Grid item xs={12} sm={7} >
                 <Posts />
             </Grid>
