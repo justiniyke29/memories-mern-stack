4d87c9b96eea6aaa4b7bedf55a8da70f6e8e74b1
justiniyke29 <106402840+justiniyke29@users.noreply.github.com>
2022-11-05 01:08:04 +0100

    added like and delete button functionality.

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -4,11 +4,15 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
+import { useDispatch} from 'react-redux';
+import { deletePost, likePost} from '../../../actions/posts';
 
 import useStyles from './styles';
 
-const Post = ({ post }) => {
+const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
   return (
     <Card className={classes.card}>
       <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -17,23 +21,27 @@ const Post = ({ post }) => {
         <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
       </div>
       <div className={classes.overlay2}>
-        <Button style={{color: 'white'}} size='small' onClick={() =>{}}>
-          <MoreHorizIcon fontSize='default'/>
+        <Button 
+        style={{color: 'white'}} 
+        size='small' 
+        onClick={() => setCurrentId(post._id)}>
+          <MoreHorizIcon fontSize='medium'/>
         </Button>
       </div>
       <div className={classes.details}>
         <Typography variant='body2' color='textSecondary'>{post.tags.map((tag) => `#${tag} `)}</Typography>
       </div>
+        <Typography className={classes.title} variant='h5' gutterBottom >{post.title}</Typography>
       <CardContent>
-        <Typography className={classes.title} variant='h5' gutterBottom={true} >{post.message}</Typography>
+        <Typography variant='body2' color='textSecondary' component='p' >{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size='small' color='primary' onClick={() => {}} >
+        <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))} >
           <ThumbUpAltIcon fontSize='small' />
           Like
           {post.likeCount}
         </Button>
-        <Button size='small' color='primary' onClick={() => {}} >
+        <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))} >
           <DeleteIcon fontSize='small' />
           Delete
         </Button>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -30,7 +30,27 @@ export const updatePost = async (req, res) => {
 
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with such id');
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new: tue});
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, {new: true});
     
     res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+    const { id: _id} =req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with such id');
+    await PostMessage.findByIdAndRemove( _id);
+
+    res.json({message: 'Post deleted successfully'})
+}
+
+export const likePost = async (req, res) => {
+    const { id: _id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with such id');
+
+    const post = await PostMessage.findById(_id);
+    const updatePost = await PostMessage.findByIdAndUpdate(_id, { likeCount: post.likeCount + 1 }, {new: true});
+
+    res.json(updatePost);
 }
